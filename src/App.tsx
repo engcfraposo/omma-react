55703506de5244904a84f59cf40e9753dabb9879
Cláudio Rapôso <engcfraposo@gmail.com>
2022-06-02 21:24:53 -0300

    initial_commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      {/** Header */}
+      <Header />
+      {/** ContentList */}
+      {/** Footer */}
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from "../../assets/logo.png"
+import "./styles.css"
+
+const Footer: React.FC = () => {
+  return (
+      <footer>
+          <img src={logo} alt="omma" />
+          <span>
+            © 2022 omma - Todos os direitos são de vocês
+          </span>
+      </footer>
+  );
+}
+
+export default Footer;
