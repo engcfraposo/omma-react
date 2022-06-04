4ec3069d81e2a490e30dae5953ee73bb19c5a049
Cláudio Rapôso <engcfraposo@gmail.com>
2022-06-04 13:15:33 -0300

    update

--- src/components/ContentList/index.tsx
+++ src/components/ContentList/index.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import risoto from "../../assets/risoto.png"
+import bolo from "../../assets/bolo.png"
+import tabule from "../../assets/tabule.png"
+import moqueca from "../../assets/moqueca.png"
 import "./styles.css"
 
+interface Recipes {
+    id: number,
+    nome: string,
+    img: string,
+}
+
+const recipes: Recipes[] = [
+    {id:1, nome:"risoto de abobora", img:risoto},
+    {id:2, nome:"bolo de banana", img:bolo},
+    {id:3, nome:"tabule tradicional", img:tabule},
+    {id:4, nome:"moqueca", img:moqueca},
+]
+
 const ContentList: React.FC = () => {
+const [recipesList, setRecipes] = useState<Recipes[]>(recipes) 
+    const handleAddRecipe = () => {
+        setRecipes(
+            (oldRecipeList) =>
+            [...oldRecipeList, {
+                id: oldRecipeList.length + 1,
+                nome: "Gororoba",
+                img: tabule,
+            }]
+        )
+    }
   return (
       <main className="content-container">
           <h1>todas as receitas</h1>
           <div className="grid-container">
-              {[1,2,3,4].map(_qty=>(
-                  <div className="grid-item">
+              {recipesList.map(recipeUnit=>(
+                  <div key={recipeUnit.id} className="grid-item">
                       <div className="card-container">
-                        <img src={risoto} alt="risoto" />
-                        <span>Risoto</span>
+                        <img src={recipeUnit.img} alt={recipeUnit.nome} />
+                        <span>{recipeUnit.nome}</span>
                         <button>Conferir</button>
                       </div>
                   </div>
               ))}
           </div>
+          <button onClick={handleAddRecipe}>Adicionar Receita</button>
       </main>
   );
 }
